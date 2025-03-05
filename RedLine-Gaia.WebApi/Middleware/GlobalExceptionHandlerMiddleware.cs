90cdc798a9f5eaa4b076dfa964c977e9e44999e0
TheCodingWaffle <stephen@vindevcorp.com>
2025-03-05 14:39:42 -0600

    Serilog and Betterstack configured, updated readme, and simple logging added to global exception

--- RedLine-Gaia.WebApi/Middleware/GlobalExceptionHandlerMiddleware.cs
+++ RedLine-Gaia.WebApi/Middleware/GlobalExceptionHandlerMiddleware.cs
@@ -2,10 +2,14 @@ using FluentResults;
 using FluentValidation;
 using RedLine_Gaia.Application.ResultDto;
 using RedLine_Gaia.Domain.Errors;
+using Serilog.Context;
 
 namespace RedLine_Gaia.WebApi.Middleware;
 
-public class GlobalExceptionHandlerMiddleware(RequestDelegate next)
+public class GlobalExceptionHandlerMiddleware(
+    RequestDelegate next,
+    ILogger<GlobalExceptionHandlerMiddleware> logger
+)
 {
     public async Task InvokeAsync(HttpContext context)
     {
@@ -15,17 +19,18 @@ public class GlobalExceptionHandlerMiddleware(RequestDelegate next)
         }
         catch (Exception ex)
         {
-            // Add logger logic here to log thrown exception.
-
             if (ex is ValidationException validationException)
             {
                 var errors = validationException.Errors.Select(x => x.ErrorMessage);
+                logger.LogWarning("Validation Exception {@errors}", errors);
+
                 context.Response.ContentType = "application/json";
                 context.Response.StatusCode = 200;
                 await context.Response.WriteAsJsonAsync(Result.Fail(errors).ToResultDto());
             }
             else
             {
+                logger.LogError("Internal Service Error thrown with exception {ex}", ex);
                 context.Response.ContentType = "application/json";
                 context.Response.StatusCode = 500;
                 await context.Response.WriteAsJsonAsync(
--- RedLine-Gaia.WebApi/Program.cs
+++ RedLine-Gaia.WebApi/Program.cs
@@ -7,11 +7,53 @@ using RedLine_Gaia.Infrastructure.Database;
 using RedLine_Gaia.Infrastructure.Services;
 using RedLine_Gaia.WebApi.Middleware;
 using Scalar.AspNetCore;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+var betterStackSourceToken = builder.Configuration.GetValue<string>("BetterStack:sourceToken");
+var betterStackEndpoint = builder.Configuration.GetValue<string>("BetterStack:betterStackEndpoint");
+
+if (!string.IsNullOrEmpty(betterStackSourceToken) && !string.IsNullOrEmpty(betterStackEndpoint))
+{
+    var levelSwitch = new LoggingLevelSwitch();
+
+    LogEventLevel minimumLogLevel = Enum.TryParse(
+        typeof(LogEventLevel),
+        builder.Configuration.GetValue("BetterStack:logLevel", "Information"),
+        out object? logLevel
+    )
+        ? (LogEventLevel)logLevel!
+        : LogEventLevel.Information;
+
+    levelSwitch.MinimumLevel = minimumLogLevel;
+
+    builder.Host.UseSerilog(
+        (context, loggerConfiguration) =>
+        {
+            loggerConfiguration
+                .MinimumLevel.ControlledBy(levelSwitch)
+                .WriteTo.BetterStack(
+                    sourceToken: betterStackSourceToken,
+                    betterStackEndpoint: betterStackEndpoint
+                );
+        }
+    );
+}
+else
+{
+    builder.Host.UseSerilog(
+        (context, loggerConfiguration) =>
+        {
+            loggerConfiguration.MinimumLevel.Information().WriteTo.Console();
+        }
+    );
+}
 
 builder.Services.AddScoped<ICurrentTenantService, CurrentTenantService>();
 
@@ -37,6 +79,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseSerilogRequestLogging();
 app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
 app.UseHttpsRedirection();
 app.UseAuthorization();
